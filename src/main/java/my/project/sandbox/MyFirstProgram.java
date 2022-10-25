73e9557cf03fe27c77d7db5b14f605d5f849991d
Vadim Tsanava <tsanavavadim@yandex.ru>
2022-10-25 15:24:50 +0400

    variables

--- src/main/java/my/project/sandbox/MyFirstProgram.java
+++ src/main/java/my/project/sandbox/MyFirstProgram.java
@@ -4,5 +4,8 @@ public class MyFirstProgram {
 
   public static void main(String[] args) {
     System.out.println("Hello, world!");
+int l =8;
+int s = l *l;
+    System.out.println("Площадь квадрата со стороной " + l + " = " + s);
   }
 }
