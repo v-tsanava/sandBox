f1c678278efc01a8bd436b4c64c1abba95246288
Vadim Tsanava <tsanavavadim@yandex.ru>
2022-10-26 15:12:05 +0400

    value and variable types

--- src/main/java/my/project/sandbox/MyFirstProgram.java
+++ src/main/java/my/project/sandbox/MyFirstProgram.java
@@ -3,9 +3,10 @@ package my.project.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    System.out.println("Hello, world!");
-int l =8;
-int s = l *l;
+    String somebody ="world";
+    System.out.println("Hello, " + somebody + "!");
+double l =8.0;
+double s = l *l;
     System.out.println("Площадь квадрата со стороной " + l + " = " + s);
   }
 }
