3d0570ee81a6d472932c3b1afd0e8fd867c85860
Vadim Tsanava <tsanavavadim@yandex.ru>
2022-10-27 19:17:58 +0400

    2.8

--- src/main/java/my/project/sandbox/MyFirstProgram.java
+++ src/main/java/my/project/sandbox/MyFirstProgram.java
@@ -3,10 +3,22 @@ package my.project.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    String somebody ="world";
-    System.out.println("Hello, " + somebody + "!");
-double l =8.0;
-double s = l *l;
-    System.out.println("Площадь квадрата со стороной " + l + " = " + s);
+    hello("world");
+    hello("salam");
+
+    Square s = new Square(5);
+    System.out.println("Площадь квадрата со стороной " + s.l + "=" + area(s));
+
+    Rectangle r = new Rectangle(4,6);
+    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + area(r));
+    }
+    public static void hello (String somebody) {
+      System.out.println("Hello, " + somebody + "!");
+    }
+    public static double area (Square s) {
+    return  s.l * s.l;
+    }
+    public static double area (Rectangle r){
+    return r.a * r.b;
+    }
   }
-}
---
+++ src/main/java/my/project/sandbox/Square.java
@@ -0,0 +1,8 @@
+package my.project.sandbox;
+
+public class Square {
+  public double l;
+  public Square(double l){
+    this.l = l;
+  }
+}
